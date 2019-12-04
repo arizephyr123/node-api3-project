fd1160b7309ab2878543b6e25e39b1dd3d60c2d6
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-04 12:12:33 -0800

    Initial Commit, server created, logger middleware fn created, functional

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require ('./server.js');
+
+const port = 4003;
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+
+// endpoints
+//server.use(".api/users")
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +19,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl} at ${Date.now()}`);
+  next();
+}
 
 module.exports = server;
